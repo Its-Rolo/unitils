662301ad6424742d400a2ea85854cdfdfbd67572
Its-Rolo <emonkey178@gmail.com>
2024-08-29 23:07:28 -0400

    update main.py for conf path

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def parse_time(time_str):
 
 def load_config():
     config = configparser.ConfigParser()
-    config.read('unitils.conf')
+    config.read('./unitils.conf')
     for section in config.sections():
         class_name = config[section]['name']
         classes.append(class_name)
